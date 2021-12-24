c8c8ca4a5a4614218d7be96dd1a4ce981394cf68
unknown <jasdeepsaini30@gmail.com>
2021-12-24 01:09:31 -0500

    adding record

--- public/js/idb.js
+++ public/js/idb.js
@@ -35,3 +35,49 @@ function saveRecord(record) {
     budgetObjectStore.add(record);
 };
 
+// function that will handle collecting all of the data 
+function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    // get all transactions from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
+          // access the object store
+          const budgetObjectStore = transaction.objectStore('new_transaction');
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
