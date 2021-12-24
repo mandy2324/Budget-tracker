5ae3876544f910499af3ce88a9029b3ee855e364
unknown <jasdeepsaini30@gmail.com>
2021-12-23 23:54:23 -0500

    retreving info from service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -23,3 +23,45 @@ self.addEventListener('install', function (e) {
         })
       )
   });
+
+  // activate a service worker
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        });
+            cacheKeeplist.push(CACHE_NAME);
+            // returns a promise that resolves once all old versions of the cache have been deleted 
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                console.log('deleting cache : ' + keyList[i] );
+                return caches.delete(keyList[i]);
+                }
+            })
+        );
+    })
+    )
+  });
+  
+  // retrieving information from the cache 
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+        //   console.log(e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+        //   console.log(e.request.url)
+          return fetch(e.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  });
+  
