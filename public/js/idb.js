2388cae4fcec45d6f57d72a80cd52b8d993f4351
unknown <jasdeepsaini30@gmail.com>
2021-12-28 16:54:01 -0500

    bug fixes config code

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,6 +5,8 @@ const request = indexedDB.open('budget_tracker', 1);
 
 // this event will emit if the database version changes
 request.onupgradeneeded = function(event) {
+  console.log('Upgrade needed in IndexDB');
+
     const db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
